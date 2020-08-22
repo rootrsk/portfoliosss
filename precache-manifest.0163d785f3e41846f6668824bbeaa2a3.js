afbf83c8c4f7f17070d165a343f892a5133c8758
RAVISHANKAR KUMA <39592401+rootrsk@users.noreply.github.com>
2020-08-22 12:00:36 +0530

    Add files via upload

---
+++ precache-manifest.0163d785f3e41846f6668824bbeaa2a3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903bd8ba1fcbd86c05b381528ca09acf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "95797a9833fac80e66d9",
+    "url": "/portfolio/static/css/main.2d5c5cff.chunk.css"
+  },
+  {
+    "revision": "cf73cb6f9a7c4688d670",
+    "url": "/portfolio/static/js/2.a05686d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.a05686d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95797a9833fac80e66d9",
+    "url": "/portfolio/static/js/main.464dd79b.chunk.js"
+  },
+  {
+    "revision": "af576cafede080114400",
+    "url": "/portfolio/static/js/runtime-main.38e2fad2.js"
+  },
+  {
+    "revision": "980ef57fb9d73f29dbf205fc6b1aacfb",
+    "url": "/portfolio/static/media/Node.980ef57f.png"
+  },
+  {
+    "revision": "6d11f024003dd091cdba159dcaff1ca7",
+    "url": "/portfolio/static/media/css.6d11f024.png"
+  },
+  {
+    "revision": "1443fe76a52e2f31abd4711774228822",
+    "url": "/portfolio/static/media/express.1443fe76.png"
+  },
+  {
+    "revision": "5695b879efa92a40246cb010532a947f",
+    "url": "/portfolio/static/media/html.5695b879.png"
+  },
+  {
+    "revision": "34c325ff3b99a46b61f55a1a0c04967f",
+    "url": "/portfolio/static/media/javascript.34c325ff.png"
+  },
+  {
+    "revision": "f0f9f104169bf473b311a55940e22116",
+    "url": "/portfolio/static/media/profile.f0f9f104.jpg"
+  },
+  {
+    "revision": "37728de5836e206796a35da671696494",
+    "url": "/portfolio/static/media/reactLo.37728de5.png"
+  },
+  {
+    "revision": "1a947147e7fbdb735ef1343208e0f028",
+    "url": "/portfolio/static/media/socket.1a947147.png"
+  }
+]);
